3ce1f3ed4ec2a085f1fc865788b2c5c424e2d8cc
Martin Lavallee <44976592+mdlavallee92@users.noreply.github.com>
2023-03-01 12:38:12 -0500

    add preview function

--- R/search.R
+++ R/search.R
@@ -30,6 +30,8 @@ listMeta <- function(vault, dir) {
 
 }
 
+# Contents ------------------
+
 #' List contents of vault
 #' @param vault a vault object
 #' @export
@@ -38,3 +40,23 @@ listContents <- function(vault) {
   purrr::map_dfr(repos, ~listMeta(vault, dir = .x))
 }
 
+# Preview --------------------
+#' Preview file in Rstudio viewer
+#' @param vault a vault object
+#' @param item the item in the vault to preview
+#' @export
+preview <- function(vault, item) {
+  dd <- gh_getDirReadMe(vault, item)$download_url
+  txt <- downloadVault(dd)
+  #replace first header with % to fit pandoc
+  txt[1] <- gsub("#", "%", txt[1])
+  #create a temp dir for readme file
+  tempDir <- tempfile()
+  dir.create(tempDir)
+  tmpRmd <- file.path(tempDir, "README.Rmd")
+  #write file to tmp
+  readr::write_lines(txt, file = tmpRmd)
+  tmpHtml <- rmarkdown::render(tmpRmd, params = "ask", quiet = TRUE)
+  rstudioapi::viewer(tmpHtml)
+  invisible(tmpHtml)
+}
